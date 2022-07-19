d1e5d797c7c296d4d67c3c182196ab014c92353b
doantung2001 <clonenewby2000@gmail.com>
2022-07-19 13:57:11 +0700

    view website

---
+++ resources/views/layout/slider.blade.php
@@ -0,0 +1,83 @@
+@extends('layout.header')
+@section('slider')
+    <section class="ranna-slider-area">
+        <div class="container">
+            <div class="rc-carousel nav-control-layout2" data-loop="true" data-items="30" data-margin="5"
+                 data-autoplay="false" data-autoplay-timeout="5000" data-smart-speed="700" data-dots="false"
+                 data-nav="true" data-nav-speed="false" data-r-x-small="1" data-r-x-small-nav="true"
+                 data-r-x-small-dots="false" data-r-x-medium="1" data-r-x-medium-nav="true" data-r-x-medium-dots="false"
+                 data-r-small="1" data-r-small-nav="true" data-r-small-dots="false" data-r-medium="1"
+                 data-r-medium-nav="true" data-r-medium-dots="false" data-r-large="1" data-r-large-nav="true"
+                 data-r-large-dots="false" data-r-extra-large="1" data-r-extra-large-nav="true"
+                 data-r-extra-large-dots="false">
+                <div class="ranna-slider-content-layout1">
+                    <figure class="item-figure"><a href="{{----}}single-recipe1.html"><img src="{{asset('themes/img/slider/slide1-1.jpg')}}" alt="Product"></a></figure>
+
+                    <div class="item-content">
+                        <span class="sub-title">SALAD</span>
+                        <h2 class="item-title"><a href="{{----}}single-recipe1.html">Italiano Salad Mixed</a></h2>
+                        <ul class="item-rating">
+                            <li class="star-fill"><i class="fas fa-star"></i></li>
+                            <li class="star-fill"><i class="fas fa-star"></i></li>
+                            <li class="star-fill"><i class="fas fa-star"></i></li>
+                            <li class="star-fill"><i class="fas fa-star"></i></li>
+                            <li class="star-empty"><i class="fas fa-star"></i></li>
+                            <li><span>9<span> / 10</span></span> </li>
+                        </ul>
+                        <p>More off this less hello salamander lied porpoise much over tightly circa
+                            outside crud mightily rigorouse. </p>
+                        <ul class="entry-meta">
+                            <li><a href="#"><i class="fas fa-clock"></i>15 Mins</a></li>
+                            <li><a href="#"><i class="fas fa-user"></i>by <span>John Martin</span></a></li>
+                            <li><a href="#"><i class="fas fa-heart"></i><span>02</span> Likes</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="ranna-slider-content-layout1">
+                    <figure class="item-figure"><a href="{{----}}single-recipe1.html"><img src="{{asset('themes/img/slider/slide1-2.jpg')}}" alt="Product"></a></figure>
+                    <div class="item-content">
+                        <span class="sub-title">SALAD</span>
+                        <h2 class="item-title"><a href="{{----}}single-recipe1.html">Italiano Salad Mixed</a></h2>
+                        <ul class="item-rating">
+                            <li class="star-fill"><i class="fas fa-star"></i></li>
+                            <li class="star-fill"><i class="fas fa-star"></i></li>
+                            <li class="star-fill"><i class="fas fa-star"></i></li>
+                            <li class="star-fill"><i class="fas fa-star"></i></li>
+                            <li class="star-empty"><i class="fas fa-star"></i></li>
+                            <li><span>9<span> / 10</span></span> </li>
+                        </ul>
+                        <p>More off this less hello salamander lied porpoise much over tightly circa
+                            outside crud mightily rigorouse. </p>
+                        <ul class="entry-meta">
+                            <li><a href="#"><i class="fas fa-clock"></i>15 Mins</a></li>
+                            <li><a href="#"><i class="fas fa-user"></i>by <span>John Martin</span></a></li>
+                            <li><a href="#"><i class="fas fa-heart"></i><span>02</span> Likes</a></li>
+                        </ul>
+
+                </div>
+                <div class="ranna-slider-content-layout1">
+                    <figure class="item-figure"><a href="{{----}}single-recipe1.html"><img src="{{asset('themes/img/slider/slide1-3.jpg')}}" alt="Product"></a></figure>
+                    <div class="item-content">
+                        <span class="sub-title">SALAD</span>
+                        <h2 class="item-title"><a href="{{----}}single-recipe1.html">Italiano Salad Mixed</a></h2>
+                        <ul class="item-rating">
+                            <li class="star-fill"><i class="fas fa-star"></i></li>
+                            <li class="star-fill"><i class="fas fa-star"></i></li>
+                            <li class="star-fill"><i class="fas fa-star"></i></li>
+                            <li class="star-fill"><i class="fas fa-star"></i></li>
+                            <li class="star-empty"><i class="fas fa-star"></i></li>
+                            <li><span>9<span> / 10</span></span> </li>
+                        </ul>
+                        <p>More off this less hello salamander lied porpoise much over tightly circa
+                            outside crud mightily rigorouse. </p>
+                        <ul class="entry-meta">
+                            <li><a href="#"><i class="fas fa-clock"></i>15 Mins</a></li>
+                            <li><a href="#"><i class="fas fa-user"></i>by <span>John Martin</span></a></li>
+                            <li><a href="#"><i class="fas fa-heart"></i><span>02</span> Likes</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
